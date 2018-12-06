074c55d11fc5a123a79530c774f163a153b602be
Omar <omarabdeljelil@gmail.com>
2018-12-06 17:21:29 -0500

    adding react icons

--- ski-app/src/components/SkiDayCount.js
+++ ski-app/src/components/SkiDayCount.js
@@ -1,20 +1,40 @@
-import React from 'react'
 import '../stylesheets/ui.scss'
+import { FaCalendarAlt } from 'react-icons/fa'
+import { FaSnowflake } from 'react-icons/fa'
+import { FaMountain } from 'react-icons/fa'
 
-export const SkiDayCount = React.createClass({
-	render() {
-		return (
-			<div className="ski-day-count">
-				<div className="total-days">
-					<span>5 days</span>
-				</div>
-				<div className="powder-days">
-					<span>2 days</span>
-				</div>
-				<div className="backcountry-days">
-					<span>1 hiking day</span>
-				</div>
+const percentToDecimal = (decimal) => {
+	return ((decimal * 100) + '%')
+}
+
+const calcGoalProgress = (total, goal) => {
+	return percentToDecimal(total/goal)
+}
+
+export const SkiDayCount = ({total, powder, backcountry, goal}) => (
+		<div className="ski-day-count">
+			<div className="total-days">
+				<span>{total}</span>
+					<FaCalendarAlt />
+				<span>days</span>
 			</div>
-		)
-	}
-})
+			<div className="powder-days">
+				<span>{powder}</span>
+					<FaSnowflake />
+				<span>days</span>
+			</div>
+			<div className="backcountry-days">
+				<span>{backcountry}</span>
+					<FaMountain />
+				<span>days</span>
+			</div>
+			<div>
+				<span>
+					{calcGoalProgress(
+						total, 
+						goal
+					)}
+				</span>
+			</div>
+		</div>
+)
--- ski-app/src/index.js
+++ ski-app/src/index.js
@@ -5,6 +5,9 @@ import { SkiDayCount } from './components/SkiDayCount'
 window.React = React
 
 render(
-	<SkiDayCount />,
+	<SkiDayCount total={50}
+				 powder={20}
+				 backcountry={10}
+				 goal={100}/>,
 	document.getElementById('react-container')
 )
